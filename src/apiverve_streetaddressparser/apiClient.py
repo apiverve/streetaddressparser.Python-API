8cac7afe3c6b617d29583557a4927aac8d0c69fc
APIVerve Team <hello@apiverve.com>
2025-03-28 17:53:08 -0500

    Bug Fixes

--- src/apiverve_streetaddressparser/apiClient.py
+++ src/apiverve_streetaddressparser/apiClient.py
@@ -10,10 +10,10 @@ class StreetaddressparserAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/streetaddressparser'
+        self.base_url = 'https://api.apiverve.com/v1/streetaddressparser'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
